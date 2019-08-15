c2c8fb60f7855f9e4f84404dd2c7357ddcab8d18
iwen <liuxingyu@sxt.cn>
2019-08-15 09:24:49 +0800

    第一版

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/product_api': {
+                // target: 'http://121.42.14.194:9011',
+                target: 'http://192.168.1.102:9011',
+                pathRewrite: {
+                    '^/product_api': ''
+                },
+                changeOrigin: true
+            },
+            '/content_api': {
+                target: 'http://192.168.1.102:9021',
+                // target: 'http://121.42.14.194:9021',
+                pathRewrite: {
+                    '^/content_api': ''
+                },
+                changeOrigin: true
+            },
+            "/api":{
+                target: 'http://localhost:3000',
+                pathRewrite: {
+                    '^/api': ''
+                },
+                changeOrigin: true
+            }
+        }
+    }
+}
